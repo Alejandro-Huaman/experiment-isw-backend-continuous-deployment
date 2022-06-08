e010eef36e4c5d6e2a34e0e495a81b7c8e8e8fef
Alejandro-Huaman <90795926+Alejandro-Huaman@users.noreply.github.com>
2022-06-08 01:06:38 -0500

    first commit

---
+++ src/main/java/com/example/fortlomisw/backend/resource/Event/CreateEventResource.java
@@ -0,0 +1,19 @@
+package com.example.fortlomisw.backend.resource.Event;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+public class CreateEventResource {
+
+    private String eventname;
+    private String eventeescription;
+    private Long eventlikes;
+    private Date registerdate;
+    private String ticketLink;
+
+
+}
---
+++ src/main/java/com/example/fortlomisw/backend/resource/Forum/ForumResource.java
@@ -0,0 +1,15 @@
+package com.example.fortlomisw.backend.resource.Forum;
+
+import com.example.fortlomisw.backend.resource.Person.PersonResource;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ForumResource {
+    private Long id;
+    private String forumname;
+    private String forumdescription;
+    private String conductrules;
+    private PersonResource person;
+}
